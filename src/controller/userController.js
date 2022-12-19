d6b1bbc49874de941dc613867e3364fdac5898be
comrade31 <atulsinghgga98@gmail.com>
2022-12-19 16:16:53 +0530

    get api

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,1 +1,23 @@
-// userapi
+const userModel = require("../model/userModel")
+const { isIdValid, isValidString, isValidISBN, isValidDate, isValidName,isValidImage } = require("../validators/validator")
+
+
+     //<<<<<<<<------------------- Get-User-Api -------------------->>>>>>>>>>>>>
+
+     const getUserProfile = function(req,res) {
+        try{
+             let userId = req.params.userId
+             if(!userId) {
+                return res.status(400).send({status:false,message:"Please provide userId"})
+             }
+    
+             const findUser = userModel.findbyId(userId)
+             if(!findUser) {
+                return res.status(404).send({status:false,message:"User not found"})
+             }
+             res.status(200).send({status:true,message:"User profile details","data":findUser})
+        }
+        catch(err){
+    res.status(500).send({status:false,message:err.message})
+        }
+    }
