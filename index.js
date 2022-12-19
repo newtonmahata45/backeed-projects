1acba392a43ef80de23f91e07f84a5e2533fccb0
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2022-12-19 18:10:02 +0530

    create user api

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 
 mongoose.connect("mongodb+srv://group22:1234@group22databse.uvtoalh.mongodb.net/group9Database",
-{useNewUrlParser:true})
+{useNewUrlParser:true},mongoose.set('strictQuery', false))
 .then(()=> console.log("MongoDb is connected"))
 .catch((err)=> console.log(err) )
  
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,75 @@
 const userModel = require("../model/userModel")
-const { isIdValid, isValidString, isValidISBN, isValidDate, isValidName,isValidImage } = require("../validators/validator")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const aws = require("../aws")
 
+const { isIdValid, isValidString, isValidISBN, isValidDate, isValidName,isValidImage } = require("../validator/validator")
+
+
+const registerUser = async (req, res) => {
+   try {
+       const data = req.body
+       const profileImage = req.files
+  
+//  CHECK  if request body is empty
+       if (!Object.keys(data).length > 0) return res.status(400).send({ status: false, error: "Please enter data" })
+//------------------
+// if (!validator.isValidType(data.fname)) return res.status(400).send({ status: false, message: 'please provide first name' })
+// if (!validator.isValidType(data.lname)) return res.status(400).send({ status: false, message: 'please provide last name' })
+// if (!validator.isValidType(data.address)) return res.status(400).send({ status: false, message: 'please provide Address' })
+
+// let { shipping, billing } = data.address
+
+// if (!validator.isValidType(billing)) return res.status(400).send({ status: false, message: 'please provide billing Address'})
+// if (!validator.isValidType(billing.street)) return res.status(400).send({ status: false, message: 'please provide billing street' })
+// if (!validator.isValidType(billing.city)) return res.status(400).send({ status: false, message: 'please provide billing city' })
+if (!validator.isValidType(billing.city)) return res.status(400).send({ status: false, message: 'please provide billing city' })
+
+let { phone, email, password } = data
+       const isPhoneAlreadyUsed = await userModel.findOne({ phone })
+       if (isPhoneAlreadyUsed) return res.status(400).send({ status: false, message: "This mobile is number already in use,please provide another mobile number" })
+
+       const isEmailAlreadyUsed = await userModel.findOne({ email })
+       if (isEmailAlreadyUsed) return res.status(400).send({ status: false, message: "This  is email already in use,please provide another email" })
+
+
+       // ENCRYPTING PASSWORD
+       let saltRounds = 10;
+       let salt = await bcrypt.genSalt(saltRounds);
+       let hash = await bcrypt.hash(password, salt);
+
+       password = hash
+
+       //  Create : aws link for profile image
+       if (profileImage && profileImage.length > 0) var uploadedFileURL = await aws.uploadFile(profileImage[0])
+       else return res.status(400).send({ status: false, message: 'please provide profile image' })
+
+       //  CREATE :  user
+       const user = {
+           fname: data.fname,
+           lname: data.lname,
+           email: email,
+           profileImage: uploadedFileURL,
+           phone: phone,
+           password: password,
+           address: {
+               shipping: {
+                   street: address.shipping.street,
+                   city: address.shipping.city,
+                   pincode: address.shipping.pincode
+               },
+               billing: {
+                   street: address.billing.street,
+                   city: address.billing.city,
+                   pincode: address.billing.pincode
+               }
+           }
+       }
+       const createUser= await userModel.create(user)
+       return res.status(201).send({status: true, message: "User created successfully",data:createUser})
+   }catch(err){
+       return res.status(500).send({status: false, message:err.message})
+   }}
 
      //<<<<<<<<------------------- Get-User-Api -------------------->>>>>>>>>>>>>
 
@@ -28,4 +97,5 @@ const { isIdValid, isValidString, isValidISBN, isValidDate, isValidName,isValidI
 
 
 
-    module.exports.getUserProfile = getUserProfile
+    module.exports= {getUserProfile,registerUser}
+    
