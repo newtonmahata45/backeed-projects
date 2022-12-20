26526a9114b1485d2aa16c768159253566567c17
Dipesh-J <dipeshjoshi015@gmail.com>
2022-12-20 17:26:06 +0530

    regex added for get

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -5,10 +5,17 @@ const productModel = require("../model/productModel")
 exports.getProducts = async(req,res)=>{
     if(Object.keys(req.query).length == 0){
     const products = await productModel.find({isDeleted:false})
+    return res.status(200).send({status:true,message:"Searched Results",data:products})
     }
     if(Object.keys(req.query).length > 0){
         req.query.isDeleted = false
-        const filteredProducts = await productModel.find(req.query)
+        let size = req.query.size//.toUpperCase() //availableSizes
+        let name = req.query.name // title
+
+        const filteredProducts = await productModel.find({$or:[{availableSizes:{$in:size}},{title:{$regex:name}}]})
+
+        if(!filteredProducts) return res.status(400).send({status:false,message:"No Results Found"})
+        return res.status(200).send({status:true,message:"Search Results",data:filteredProducts})
     }
 }
 
